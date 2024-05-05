743a6efc886847a2083e38f972c4b0850bb81b2d
solowins <stamuno9@gmail.com>
2024-05-05 13:06:05 +0100

    update

--- 28. Show Hide Password/app.js
+++ 28. Show Hide Password/app.js
@@ -13,5 +13,5 @@ eye.addEventListener("click", () => {
 });
 
 eye.addEventListener("click", () => {
-    eye.style.color === "rgb(10, 10, 165)" ? eye.style.color = "" : eye.style.color = "rgb(10, 10, 165)";
+    eye.style.color === "rgb(13, 170, 255)" ? eye.style.color = "" : eye.style.color = "rgb(13, 170, 255)";
   });
